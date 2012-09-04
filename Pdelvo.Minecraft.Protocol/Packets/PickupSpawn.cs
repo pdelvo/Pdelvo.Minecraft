pdelvo/Pdelvo.Minecraft

commit 05fd4842015c7263027702200f0e861a3c263c2e

Fixed a protocol bug

Pdelvo.Minecraft.Protocol/Packets/PickupSpawn.cs
@@ -28,19 +28,7 @@ namespace Pdelvo.Minecraft.Protocol.Packets
         /// </summary>
         /// <value>The item.</value>
         /// <remarks></remarks>
-        public short Item { get; set; }
-        /// <summary>
-        /// Gets or sets the count.
-        /// </summary>
-        /// <value>The count.</value>
-        /// <remarks></remarks>
-        public byte Count { get; set; }
-        /// <summary>
-        /// Gets or sets the damage data.
-        /// </summary>
-        /// <value>The damage data.</value>
-        /// <remarks></remarks>
-        public short DamageData { get; set; }
+        public ItemStack Item { get; set; }
         /// <summary>
         /// Gets or sets the X.
         /// </summary>
@@ -89,9 +77,7 @@ namespace Pdelvo.Minecraft.Protocol.Packets
             if (reader == null)
                 throw new System.ArgumentNullException("reader");
             EntityId = reader.ReadInt32();
-            Item = reader.ReadInt16();
-            Count = reader.ReadByte();
-            DamageData = reader.ReadInt16();
+            Item = ItemStack.Read(reader);
             PositionX = reader.ReadInt32();
             PositionY = reader.ReadInt32();
             PositionZ = reader.ReadInt32();
@@ -113,8 +99,6 @@ namespace Pdelvo.Minecraft.Protocol.Packets
             writer.Write(Code);
             writer.Write(EntityId);
             writer.Write(Item);
-            writer.Write(Count);
-            writer.Write(DamageData);
             writer.Write(PositionX);
             writer.Write(PositionY);
             writer.Write(PositionZ);

commit 5171cf26c6c76663716b021e4d3e0006a474fc95

Fixed a protocol bug with version older then 41 Bug was introduced in 05fd4842015c7263027702200f0e861a3c263c2e

Pdelvo.Minecraft.Protocol/Packets/PickupSpawn.cs
@@ -15,6 +15,8 @@ namespace Pdelvo.Minecraft.Protocol.Packets
         public PickupSpawn()
         {
             Code = 0x15;
+
+            Item = new ItemStack();
         }
 
         /// <summary>
@@ -77,7 +79,18 @@ namespace Pdelvo.Minecraft.Protocol.Packets
             if (reader == null)
                 throw new System.ArgumentNullException("reader");
             EntityId = reader.ReadInt32();
+            if(version >=41)
             Item = ItemStack.Read(reader);
+            else
+            {
+                Item = new ItemStack
+                {
+                    ItemType = reader.ReadInt16(),
+                    Count = reader.ReadByte(),
+                    Durability = reader.ReadInt16(),
+                    AdditionalData = new byte[0]
+                };
+            }
             PositionX = reader.ReadInt32();
             PositionY = reader.ReadInt32();
             PositionZ = reader.ReadInt32();
@@ -98,7 +111,14 @@ namespace Pdelvo.Minecraft.Protocol.Packets
                 throw new System.ArgumentNullException("writer");
             writer.Write(Code);
             writer.Write(EntityId);
+            if(version >=41)
             writer.Write(Item);
+            else
+            {
+                writer.Write(Item.ItemType);
+                writer.Write(Item.Count);
+                writer.Write(Item.Durability);
+            }
             writer.Write(PositionX);
             writer.Write(PositionY);
             writer.Write(PositionZ);