07d16f8fc702018af385a95a0a60cc596500cfc4
Dennis Fischer <fischer_dennis@live.de>
2012-09-15 22:14:49 +0200

    Added mono workaround, because monos CreateDelegate method has an other sugnature then the one in .net

--- Pdelvo.Minecraft.Protocol/PacketFactory.cs
+++ Pdelvo.Minecraft.Protocol/PacketFactory.cs
@@ -44,14 +44,41 @@ namespace Pdelvo.Minecraft.Protocol
             lock (CreateCache)
             {
                 if (CreateCache.ContainsKey(type)) return CreateCache[type];
-                var dyn = new DynamicMethod("PC_FACTORY_" + type.Name, typeof(Packet), new Type[] { });
-                ILGenerator gen = dyn.GetILGenerator();
-                gen.Emit(OpCodes.Newobj, type.GetConstructor(new Type[] { })); //Constructoraufruf
-                gen.Emit(OpCodes.Ret); //Rückgabe der neuen Instanz
-                var ret = (PacketCreator)dyn.CreateDelegate(typeof(PacketCreator));
+
+                PacketCreator ret;
+
+                if (RuntimeInfo.IsMono)
+                {
+                    ret = GetMonoCreator(type);
+                }
+                else
+                {
+                    ret = GetNetCreator(type);
+                }
                 CreateCache.Add(type, ret);
                 return ret;
             }
+        }
+
+        private static PacketCreator GetNetCreator(Type type)
+        {
+            PacketCreator ret;
+            var dyn = new DynamicMethod("PC_FACTORY_" + type.Name, typeof(Packet), new Type[] { });
+            ILGenerator gen = dyn.GetILGenerator();
+            gen.Emit(OpCodes.Newobj, type.GetConstructor(new Type[] { })); //Constructoraufruf
+            gen.Emit(OpCodes.Ret); //Rückgabe der neuen Instanz
+            ret = (PacketCreator)dyn.CreateDelegate(typeof(PacketCreator));
+            return ret;
+        }
+
+        /// <summary>
+        /// Fallback because the mono CreateDelegate method missmatches the one of the .net framework
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static PacketCreator GetMonoCreator(Type type)
+        {
+            return new PacketCreator(() => (Packet)Activator.CreateInstance(type));
         }
 
         /// <summary>
@@ -66,64 +93,126 @@ namespace Pdelvo.Minecraft.Protocol
             lock (SendCache)
             {
                 if (SendCache.ContainsKey(type)) return SendCache[type];
-                bool throwOnRequired;
-                bool throwOnLast;
 
-                int requiredVersion = PacketEndPoint.GetRequiredVersion(type, out throwOnRequired);
-                int lastSupportedVersion = PacketEndPoint.GetLastSupportedVersion(type, out throwOnLast);
+                PacketSender ret;
 
-                var dyn = new DynamicMethod("PS_FACTORY_" + type.Name, typeof(void),
-                                            new[] { typeof(BigEndianStream), typeof(int), typeof(Packet) });
-                ILGenerator gen = dyn.GetILGenerator();
-                gen.Emit(OpCodes.Ldarg_2); // Lade packet
-                gen.Emit(OpCodes.Ldarg_0); // Lade parameter BigEndianStream auf den Stack
-                gen.Emit(OpCodes.Ldarg_1); // Lade die Minecraft Version auf den Stack
-                gen.Emit(OpCodes.Ldc_I4, requiredVersion);
-                gen.Emit(OpCodes.Ldc_I4, lastSupportedVersion);
-                gen.Emit(throwOnRequired ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
-                gen.Emit(throwOnLast ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
-                gen.Emit(OpCodes.Call,
-                         typeof(PacketFactory).GetMethod("SendPacket",
-                                                          BindingFlags.Static | BindingFlags.Public |
-                                                          BindingFlags.InvokeMethod));
-                gen.Emit(OpCodes.Ret);
-                var ret = (PacketSender)dyn.CreateDelegate(typeof(PacketSender));
+                if (RuntimeInfo.IsMono)
+                {
+                    ret = GetMonoSender(type);
+                }
+                else
+                {
+                    ret = GetNetSender(type);
+                }
                 SendCache.Add(type, ret);
                 return ret;
             }
         }
+
+        private static PacketSender GetNetSender(Type type)
+        {
+            PacketSender ret;
+            bool throwOnRequired;
+            bool throwOnLast;
+
+            int requiredVersion = PacketEndPoint.GetRequiredVersion(type, out throwOnRequired);
+            int lastSupportedVersion = PacketEndPoint.GetLastSupportedVersion(type, out throwOnLast);
+
+            var dyn = new DynamicMethod("PS_FACTORY_" + type.Name, typeof(void),
+                                        new[] { typeof(BigEndianStream), typeof(int), typeof(Packet) });
+            ILGenerator gen = dyn.GetILGenerator();
+            gen.Emit(OpCodes.Ldarg_2); // Lade packet
+            gen.Emit(OpCodes.Ldarg_0); // Lade parameter BigEndianStream auf den Stack
+            gen.Emit(OpCodes.Ldarg_1); // Lade die Minecraft Version auf den Stack
+            gen.Emit(OpCodes.Ldc_I4, requiredVersion);
+            gen.Emit(OpCodes.Ldc_I4, lastSupportedVersion);
+            gen.Emit(throwOnRequired ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
+            gen.Emit(throwOnLast ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
+            gen.Emit(OpCodes.Call,
+                     typeof(PacketFactory).GetMethod("SendPacket",
+                                                      BindingFlags.Static | BindingFlags.Public |
+                                                      BindingFlags.InvokeMethod));
+            gen.Emit(OpCodes.Ret);
+            ret = (PacketSender)dyn.CreateDelegate(typeof(PacketSender));
+            return ret;
+        }
+
+        /// <summary>
+        /// Fallback because the mono CreateDelegate method missmatches the one of the .net framework
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static PacketSender GetMonoSender(Type type)
+        {
+            bool throwOnRequired;
+            bool throwOnLast;
+            int requiredVersion = PacketEndPoint.GetRequiredVersion(type, out throwOnRequired);
+            int lastSupportedVersion = PacketEndPoint.GetLastSupportedVersion(type, out throwOnLast);
+            return new PacketSender((stream, version, packet) =>SendPacket(packet, stream, version, requiredVersion, lastSupportedVersion, throwOnRequired, throwOnLast));
+        }
+
         internal static PacketSenderAsync GetSenderAsync(Type type)
         {
             if (SendCacheAsync.ContainsKey(type)) return SendCacheAsync[type];
             lock (SendCacheAsync)
             {
                 if (SendCacheAsync.ContainsKey(type)) return SendCacheAsync[type];
-                bool throwOnRequired;
-                bool throwOnLast;
 
-                int requiredVersion = PacketEndPoint.GetRequiredVersion(type, out throwOnRequired);
-                int lastSupportedVersion = PacketEndPoint.GetLastSupportedVersion(type, out throwOnLast);
-
-                var dyn = new DynamicMethod("PSA_FACTORY_" + type.Name, typeof(Task),
-                                            new[] { typeof(BigEndianStream), typeof(int), typeof(Packet) });
-                ILGenerator gen = dyn.GetILGenerator();
-                gen.Emit(OpCodes.Ldarg_2); // Lade packet
-                gen.Emit(OpCodes.Ldarg_0); // Lade parameter BigEndianStream auf den Stack
-                gen.Emit(OpCodes.Ldarg_1); // Lade die Minecraft Version auf den Stack
-                gen.Emit(OpCodes.Ldc_I4, requiredVersion);
-                gen.Emit(OpCodes.Ldc_I4, lastSupportedVersion);
-                gen.Emit(throwOnRequired ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
-                gen.Emit(throwOnLast ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
-                gen.Emit(OpCodes.Call,
-                         typeof(PacketFactory).GetMethod("SendPacketAsync",
-                                                          BindingFlags.Static | BindingFlags.Public |
-                                                          BindingFlags.InvokeMethod));
-                gen.Emit(OpCodes.Ret); //Rückgabe des Tasks
-                var ret = (PacketSenderAsync)dyn.CreateDelegate(typeof(PacketSenderAsync));
+                PacketSenderAsync ret;
+                if (RuntimeInfo.IsMono)
+                {
+                    ret = GetMonoSenderAsync(type);
+                }
+                else
+                {
+                    ret = GetNetSenderAsync(type);
+                }
                 SendCacheAsync.Add(type, ret);
                 return ret;
             }
         }
+
+        private static PacketSenderAsync GetNetSenderAsync(Type type)
+        {
+            PacketSenderAsync ret;
+            bool throwOnRequired;
+            bool throwOnLast;
+
+            int requiredVersion = PacketEndPoint.GetRequiredVersion(type, out throwOnRequired);
+            int lastSupportedVersion = PacketEndPoint.GetLastSupportedVersion(type, out throwOnLast);
+
+            var dyn = new DynamicMethod("PSA_FACTORY_" + type.Name, typeof(Task),
+                                        new[] { typeof(BigEndianStream), typeof(int), typeof(Packet) });
+            ILGenerator gen = dyn.GetILGenerator();
+            gen.Emit(OpCodes.Ldarg_2); // Lade packet
+            gen.Emit(OpCodes.Ldarg_0); // Lade parameter BigEndianStream auf den Stack
+            gen.Emit(OpCodes.Ldarg_1); // Lade die Minecraft Version auf den Stack
+            gen.Emit(OpCodes.Ldc_I4, requiredVersion);
+            gen.Emit(OpCodes.Ldc_I4, lastSupportedVersion);
+            gen.Emit(throwOnRequired ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
+            gen.Emit(throwOnLast ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
+            gen.Emit(OpCodes.Call,
+                     typeof(PacketFactory).GetMethod("SendPacketAsync",
+                                                      BindingFlags.Static | BindingFlags.Public |
+                                                      BindingFlags.InvokeMethod));
+            gen.Emit(OpCodes.Ret); //Rückgabe des Tasks
+            ret = (PacketSenderAsync)dyn.CreateDelegate(typeof(PacketSenderAsync));
+            return ret;
+        }
+        /// <summary>
+        /// Fallback because the mono CreateDelegate method missmatches the one of the .net framework
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static PacketSenderAsync GetMonoSenderAsync(Type type)
+        {
+            bool throwOnRequired;
+            bool throwOnLast;
+            int requiredVersion = PacketEndPoint.GetRequiredVersion(type, out throwOnRequired);
+            int lastSupportedVersion = PacketEndPoint.GetLastSupportedVersion(type, out throwOnLast);
+            return new PacketSenderAsync((stream, version, packet) => SendPacketAsync(packet, stream, version, requiredVersion, lastSupportedVersion, throwOnRequired, throwOnLast));
+        }
+
 
         /// <summary>
         /// Sends the packet.
