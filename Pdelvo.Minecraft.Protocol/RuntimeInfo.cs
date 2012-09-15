09c61939c9207248fc55cab349d34b76ed16becc
Dennis Fischer <fischer_dennis@live.de>
2012-09-15 22:26:02 +0200

    Added missing file

---
+++ Pdelvo.Minecraft.Protocol/RuntimeInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdelvo.Minecraft.Protocol
+{
+    /// <summary>
+    /// Get information about the runtime the library is used in
+    /// </summary>
+    public static class RuntimeInfo
+    {
+        /// <summary>
+        /// True if the runtime is mono, otherwise false
+        /// </summary>
+        public static bool IsMono
+        {
+            get
+            {
+                return Type.GetType("Mono.Runtime") != null || true;
+            }
+        }
+    }
+}
