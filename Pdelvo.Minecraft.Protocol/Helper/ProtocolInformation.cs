8ae7b63cfd1b8b7dde644daf69630779134c6841
Dennis Fischer <fischer_dennis@live.de>
2012-10-18 17:28:57 +0200

    Change max supported version constants

--- Pdelvo.Minecraft.Protocol/Helper/ProtocolInformation.cs
+++ Pdelvo.Minecraft.Protocol/Helper/ProtocolInformation.cs
@@ -13,7 +13,7 @@
         /// <summary>
         /// 
         /// </summary>
-        public const int MaxSupportedClientVersion = 46;
+        public const int MaxSupportedClientVersion = 47;
         /// <summary>
         /// 
         /// </summary>
@@ -21,6 +21,6 @@
         /// <summary>
         /// 
         /// </summary>
-        public const int MaxSupportedServerVersion = 46;
+        public const int MaxSupportedServerVersion = 47;
     }
 }
