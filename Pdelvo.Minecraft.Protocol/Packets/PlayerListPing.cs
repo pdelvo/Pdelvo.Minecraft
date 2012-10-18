67d2da3ed942fa9ad82a3877a030a7af48bdf391
Dennis Fischer <fischer_dennis@live.de>
2012-10-18 21:17:24 +0200

    Fixes read time out Fixes that the server will wait when receiving a player list ping packet from an old client.

--- Pdelvo.Minecraft.Protocol/Packets/PlayerListPing.cs
+++ Pdelvo.Minecraft.Protocol/Packets/PlayerListPing.cs
@@ -30,13 +30,19 @@ namespace Pdelvo.Minecraft.Protocol.Packets
         {
             if (reader == null)
                 throw new System.ArgumentNullException("reader");
+            var timeOut = reader.ReadTimeout;
             try
             {
+                reader.ReadTimeout = 0;
                 MagicByte = reader.ReadByte();
             }
             catch (Exception)
             {
                 MagicByte = 0;
+            }
+            finally
+            {
+                reader.ReadTimeout = timeOut;
             }
         }
 
