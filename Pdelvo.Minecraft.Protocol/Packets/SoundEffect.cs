3ef0fcb8ad1b72288b911edba451e76d3a7c20cb
Dennis Fischer <fischer_dennis@live.de>
2012-10-21 11:39:21 +0200

    Adds missing field in sound effect

--- Pdelvo.Minecraft.Protocol/Packets/SoundEffect.cs
+++ Pdelvo.Minecraft.Protocol/Packets/SoundEffect.cs
@@ -23,30 +23,41 @@ namespace Pdelvo.Minecraft.Protocol.Packets
         /// <value>The effect Id.</value>
         /// <remarks></remarks>
         public int EffectId { get; set; }
+
         /// <summary>
         /// Gets or sets the X.
         /// </summary>
         /// <value>The X.</value>
         /// <remarks></remarks>
         public int PositionX { get; set; }
+
         /// <summary>
         /// Gets or sets the Y.
         /// </summary>
         /// <value>The Y.</value>
         /// <remarks></remarks>
         public byte PositionY { get; set; }
+
         /// <summary>
         /// Gets or sets the Z.
         /// </summary>
         /// <value>The Z.</value>
         /// <remarks></remarks>
         public int PositionZ { get; set; }
+
         /// <summary>
         /// Gets or sets the sound data.
         /// </summary>
         /// <value>The sound data.</value>
         /// <remarks></remarks>
         public int SoundData { get; set; }
+
+        /// <summary>
+        /// True if the volume will not decrease if the distance between player and source is big
+        /// </summary>
+        /// <value>NoVolumeDecrease</value>
+        /// <remarks></remarks>
+        public bool NoVolumeDecrease { get; set; }
 
         /// <summary>
         /// Receives the specified reader.
@@ -63,6 +74,15 @@ namespace Pdelvo.Minecraft.Protocol.Packets
             PositionY = reader.ReadByte();
             PositionZ = reader.ReadInt32();
             SoundData = reader.ReadInt32();
+
+            if (version >= 47)
+            {
+                NoVolumeDecrease = reader.ReadBoolean();
+            }
+            else
+            {
+                NoVolumeDecrease = EffectId == 1013;
+            }
         }
 
         /// <summary>
@@ -81,6 +101,10 @@ namespace Pdelvo.Minecraft.Protocol.Packets
             writer.Write(PositionY);
             writer.Write(PositionZ);
             writer.Write(SoundData);
+            if (version >= 47)
+            {
+                writer.Write(NoVolumeDecrease);
+            }
         }
     }
 }
